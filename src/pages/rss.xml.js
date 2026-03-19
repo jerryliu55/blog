82e9248b76ba0da27377c85de0757f55abb4c745
Jerry Liu <jerryliu55@gmail.com>
2026-03-18 23:44:41 -0400

    show content in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,22 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
+const postFiles = import.meta.glob("./posts/*.md", { eager: true });
 
 export async function GET(context) {
   return rss({
     title: "Jerry Liu | Blog",
     description: "Writing by Jerry Liu",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: Object.entries(postFiles).map(([path, post]) => ({
+      title: post.frontmatter.title,
+      pubDate: new Date(post.frontmatter.pubDate),
+      description: post.frontmatter.description,
+      link: path.replace(".", "").replace(".md", "/"),
+      content: sanitizeHtml(parser.render(post.rawContent())),
+    })),
     customData: `<language>en-us</language>`,
   });
 }
